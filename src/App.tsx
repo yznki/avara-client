5f5d646664ca6b8663e3170e294e2e880e0d1d8a
yznki <yznkiswani@gmail.com>
2025-05-07 18:15:36 +0300

    Changed the look of the main branch for now.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import Layout from './components/layout';
-
 function App() {
-  return <Layout>Test</Layout>;
+  return <div className="h-screen w-screen items-center justify-center flex"> Coming Soon...</div>;
 }
 
 export default App;
