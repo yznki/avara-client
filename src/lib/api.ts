d775b4b684944a9ee99815d3109db20705ca3954
yznki <yznkiswani@gmail.com>
2025-05-12 21:29:59 +0300

    Production fix

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-export const API_BASE_URL =
-  import.meta.env.NODE_ENV === 'production' ? 'https://api.av4ra.com' : 'http://localhost:1234';
+export const API_BASE_URL = import.meta.env.PROD
+  ? 'https://api.av4ra.com'
+  : 'http://localhost:1234';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
